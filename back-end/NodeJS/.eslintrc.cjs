869a00f135242a35dc89566e89f4fde3c3140917
TR-9NF462 <mingxuanz@mail.kakari.cc>
2024-04-22 12:01:49 -0400

    Backend prompt change, db insertion change, cleanup.

--- back-end/NodeJS/.eslintrc.cjs
+++ back-end/NodeJS/.eslintrc.cjs
@@ -1,44 +1,35 @@
 module.exports = {
-    // 'plugins': [
-    //     'mocha', 
-    // ],
-    'env': {
-        'es2021': true,
-        'node': true,
-    //    'mocha': true,
+    env: {
+        es2021: true,
+        node: true,
     },
-    'extends': 'eslint:recommended',
-    'parserOptions': {
-        'ecmaVersion': 'latest',
-        'sourceType': 'module'
+    extends: "eslint:recommended",
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
     },
-    'rules': {
-        'semi': [
-            'error',
-            'always'
+    rules: {
+        semi: ["error", "always"],
+        "no-var": ["error"],
+        "prefer-const": [
+            "error",
+            {
+                destructuring: "any",
+                ignoreReadBeforeAssign: false,
+            },
         ],
-        'no-var': [
-            'error',
-        ],
-        'prefer-const': ['error', {
-            'destructuring': 'any',
-            'ignoreReadBeforeAssign': false
-        }],
-        'curly': ['error'],
-        'eqeqeq': ['error'],
-        'no-multi-spaces': ['error'],
-        'no-lone-blocks': ['error'],
-        'no-self-compare': ['error'],
-        'no-unused-expressions': ['error'],
-        'no-useless-call': ['error'],
-        'no-use-before-define': ['error'],
+        curly: ["error"],
+        eqeqeq: ["error"],
+        "no-multi-spaces": ["error"],
+        "no-lone-blocks": ["error"],
+        "no-self-compare": ["error"],
+        "no-unused-expressions": ["error"],
+        "no-useless-call": ["error"],
+        "no-use-before-define": ["error"],
+        "func-call-spacing": ["error"],
+        "no-lonely-if": ["error"],
+        "array-bracket-spacing": ["error"],
 
-        'camelcase': ['error', {properties: 'never'}],
-        'func-call-spacing': ['error'],
-        'no-lonely-if': ['error'],
-        'array-bracket-spacing': ['error'],
-
-        'no-console': ['off'],
-    }
+        "no-console": ["off"],
+    },
 };
-
--- back-end/NodeJS/language_process.mjs
+++ back-end/NodeJS/language_process.mjs
@@ -1,23 +1,22 @@
 import OpenAI from "openai";
-import pdf from 'pdf-parse/lib/pdf-parse.js';
+import pdf from "pdf-parse/lib/pdf-parse.js";
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 function completionHandler(completion, taskName, debugMode = false) {
-    if (completion.choices[0].finish_reason !== 'stop') {
+    if (completion.choices[0].finish_reason !== "stop") {
         console.log(`Abnormal termination of LLM caused by ${completion.choices[0].finish_reason}
             when ${taskName}! Quitting...`);
         return;
     }
-    console.log(debugMode ? completion.choices[0].message.content : '');
+    console.log(debugMode ? completion.choices[0].message.content : "");
     try {
         return JSON.parse(completion.choices[0].message.content);
-    }
-    catch (err) {
-        console.log(debugMode ? err : '');
-        console.log('LLM returned a bad JSON! Quitting...');
+    } catch (err) {
+        console.log(debugMode ? err : "");
+        console.log("LLM returned a bad JSON! Quitting...");
         return;
     }
 }
@@ -29,127 +28,135 @@ async function languageProcess(fileData, debugMode = false) {
             pdfData = data;
         })
         .catch((err) => {
-            console.log(debugMode ? err : '');
-            console.log('Error parsing file! Quitting...');
+            console.log(debugMode ? err : "");
+            console.log("Error parsing file! Quitting...");
             return;
         });
     const promptTag = [
-        {"role": "system", "content": `
+        {
+            role: "system",
+            content: `
             You are designed to output JSON. Don't use backticks.
             You will be provided a list of popular topics people are interested in.
             Remember those topics and find the ones that are related to the PDF file you will be provided.
             The JSON file should contain the following keys:
             related_topics
-        `},
-        {"role": "user", "content": `
+        `,
+        },
+        {
+            role: "user",
+            content: `
             List of topics:\n"""\n
-                "programming_languages",
-                "data_science_and_analytics",
-                "web_development",
-                "graphic_design",
-                "digital_marketing",
-                "photography_and_videography",
-                "language_learning",
-                "music_production",
+                "technology",
+                "design",
+                "marketing",
+                "photography_and_videology",
+                "language",
                 "cooking_and_baking",
                 "fitness_and_nutrition",
-                "personal_finance_and_investing",
-                "entrepreneurship_and_business_skills",
-                "writing_and_blogging",
-                "meditation_and_mindfulness",
-                "diy_crafts_and_projects",
-                "gardening_and_plant_care",
-                "fashion_design_and_styling",
-                "home_improvement_and_diy_repairs",
-                "yoga_and_pilates",
-                "mobile_app_development",
-                "artificial_intelligence_and_machine_learning",
-                "cybersecurity",
-                "game_development",
-                "3d_modeling_and_animation",
-                "public_speaking_and_communication_skills",
-                "psychology_and_mental_health",
-                "travel_planning_and_tips",
-                "parenting_and_childcare",
-                "career_development_and_job_searching_strategies",
-                "drawing_and_sketching"
+                "finance_and_investing",
+                "art",
+                "history",
             \n"""
-        `},
-        {"role": "user", "content": `
+            You should not give me topics that are not listed above.
+        `,
+        },
+        {
+            role: "user",
+            content: `
             PDF File:\n"""\n${pdfData.text}\n"""
-        `}
+        `,
+        },
     ];
     const promptQuiz = [
-        {"role": "system", "content": `
+        {
+            role: "system",
+            content: `
             You are designed to output JSON. Don't use backticks.
             You will be provided with a PDF file and asked to output a quiz based on it.
-            The quiz should contain 5 questions, each with 4 possible answers.
-            The JSON file should contain the following keys:
-            - quiz
-        `},
-        {"role": "user", "content": `
+            The quiz should contain 5 questions, each with 4 possible answers. You should also tell me the correct answer
+            The JSON should be structured like this:
+            {
+                quiz: [
+                    { question: string, option: [string, string], answer: string },
+                    ...,
+                ];
+            }
+        `,
+        },
+        {
+            role: "user",
+            content: `
             PDF File:\n"""\n${pdfData.text}\n"""
-        `}
+        `,
+        },
     ];
     const promptDescription = [
-        {"role": "system", "content": `
+        {
+            role: "system",
+            content: `
             You are designed to output JSON. Don't use backticks.
             You will be provided with a PDF file and asked to generate a brief description of the article.
             The JSON file should contain the following keys:
             - title
             - description
-        `},
-        {"role": "user", "content": `
+        `,
+        },
+        {
+            role: "user",
+            content: `
             PDF File:\n"""\n${pdfData.text}\n"""
-        `}
+        `,
+        },
     ];
     let completion;
     completion = await openai.chat.completions.create({
         messages: promptTag,
-        model: "gpt-3.5-turbo"
+        model: "gpt-3.5-turbo",
     });
-    let tags = completionHandler(completion, 'categorizing', debugMode);
+    let tags = completionHandler(completion, "categorizing", debugMode);
     if (!tags) {
         return;
     }
     tags = tags.related_topics; // this name depends on the LLM
     if (!tags) {
-        console.log('LLM returned unexpected category values! Quitting...');
+        console.log("LLM returned unexpected category values! Quitting...");
         return;
     }
     // console.log(debugMode ? tags : '');
     completion = await openai.chat.completions.create({
         messages: promptQuiz,
-        model: "gpt-3.5-turbo"
+        model: "gpt-3.5-turbo",
     });
-    let quiz = completionHandler(completion, 'generating quiz', debugMode);
+    let quiz = completionHandler(completion, "generating quiz", debugMode);
     if (!quiz) {
         return;
     }
     quiz = quiz.quiz; // this name depends on the LLM
     if (!quiz) {
-        console.log('LLM returned unexpected quiz values! Quitting...');
+        console.log("LLM returned unexpected quiz values! Quitting...");
         return;
     }
     // console.log(debugMode ? quiz : '');
     completion = await openai.chat.completions.create({
         messages: promptDescription,
-        model: "gpt-3.5-turbo"
+        model: "gpt-3.5-turbo",
     });
-    const metadata = completionHandler(completion, 'generating description', debugMode);
+    const metadata = completionHandler(
+        completion,
+        "generating description",
+        debugMode
+    );
     if (!quiz) {
         return;
     }
     metadata.length = pdfData.text.length;
     if (!quiz) {
-        console.log('LLM returned unexpected description values! Quitting...');
+        console.log("LLM returned unexpected description values! Quitting...");
         return;
     }
     // console.log(debugMode ? description : '');
     return [tags, quiz, metadata];
-
 }
 
-export {
-    languageProcess
-};
+export { languageProcess };
